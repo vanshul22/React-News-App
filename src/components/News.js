ada87a3c210bc9efe20a915c5f0c8db2ee85028c
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-05-09 01:38:12 +0530

    Version 1.4.1 : Added News component style.

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,24 @@ import Newsitem from './Newsitem';
 
 
 export class News extends Component {
-  render() {
-    return (
-      <>
-        News component.
-        <Newsitem/>
-      </>
-    );
-  };
+    render() {
+        return (
+            <div className='container my-3'>
+                <h2>News Monkey - Top Headlines</h2>
+                <div className="row my-4">
+                    <div className="col-md-4">
+                        <Newsitem title="mytitle1" description="my desc1" />
+                    </div>
+                    <div className="col-md-4">
+                        <Newsitem title="mytitle2" description="my desc2" />
+                    </div>
+                    <div className="col-md-4">
+                        <Newsitem title="mytitle3" description="my desc3" />
+                    </div>
+                </div>
+            </div>
+        );
+    };
 };
 
 export default News;
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 export class Newsitem extends Component {
-  render() {
-    return (
-      <div>
-        New item.js
-      </div>
-    );
-  };
+    render() {
+        // Using props in class.
+        let { title, description } = this.props;
+        return (
+            <>
+                <div className="card" style={{ width: "18rem", margin: "10px 10px" }}>
+                    <img src="/img" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text"> {description}</p>
+                        <a href="/Read More" className="btn btn-primary">Read More...</a>
+                    </div>
+                </div>
+            </>
+        );
+    };
 };
 
 export default Newsitem;
+// https://newsapi.org/v2/top-headlines?q={cricket}&apiKey={0b0027c52e6b48db86c5d26446c5a6a3}
