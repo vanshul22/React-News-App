01e8f700f65e27082786081e5ba8a587313d7c92
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-05-09 16:20:40 +0530

    Version 1.6.1 : Use fetch to fetch data from news api website.

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -5,10 +5,11 @@ export class Newsitem extends Component {
     render() {
         // Using props in class.
         let { title, description, imgUrl, url } = this.props;
+        let noneImgUrl = "https://static.wixstatic.com/media/5df28b_58a1ccf868e64f97ad17ef4f61c9e455~mv2.png/v1/fit/w_597%2Ch_352%2Cal_c/file.png";
         return (
             <>
-                <div className="card" style={{ width: "18rem", margin: "10px 10px" }}>
-                    <img src={imgUrl} className="card-img-top" alt="error" />
+                <div className="card" style={{ width: "20rem", margin: "10px 10px" }}>
+                    <img src={imgUrl?imgUrl:noneImgUrl} className="card-img-top rounded" style={{height:"11em"}} alt="error" />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text"> {description}...</p>
