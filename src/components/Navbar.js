194ba372ab5e568e528eb29caa3e349f3c445c94
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-05-17 17:14:44 +0530

    Version 2.1.2 : Added Router paths with their categories.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Navbar extends Component {
   render() {
@@ -6,9 +7,9 @@ export class Navbar extends Component {
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
               News Monkey
-            </a>
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -22,19 +23,19 @@ export class Navbar extends Component {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item"><a className="nav-link" aria-current="page" href="/">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+                <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
                 <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">News Category</a>
+                  <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">News Category</Link>
                   <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="/business">Business</a></li>
-                    <li><a className="dropdown-item" href="/entertainment"> Entertainment</a></li>
-                    <li><a className="dropdown-item" href="/general">General</a></li>
-                    <li><a className="dropdown-item" href="/health">Health </a></li>
+                    <li><Link className="dropdown-item" to="/business">Business</Link></li>
+                    <li><Link className="dropdown-item" to="/entertainment">Entertainment</Link></li>
+                    <li><Link className="dropdown-item" to="/general">General</Link></li>
+                    <li><Link className="dropdown-item" to="/health">Health</Link></li>
                     <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="/science">Science</a></li>
-                    <li><a className="dropdown-item" href="/sports">Sports</a></li>
-                    <li><a className="dropdown-item" href="/technology">Technology</a></li>
+                    <li><Link className="dropdown-item" to="/science">Science</Link></li>
+                    <li><Link className="dropdown-item" to="/sports">Sports</Link></li>
+                    <li><Link className="dropdown-item" to="/technology">Technology</Link></li>
                   </ul>
                 </li>
               </ul>
